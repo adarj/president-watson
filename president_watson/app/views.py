6b3be9553e47aa31e4c6b9d437415dc101675b3b
Akil Darjean <adarjean@uh.edu>
2016-10-30 00:32:01 -0500

    Functionality and style improvements

--- president_watson/app/views.py
+++ president_watson/app/views.py
@@ -1,13 +1,34 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
+
+
+POLITICIANS = {"realDonaldTrump": "Donald Trump (@realDonaldTrump)",
+               "HiliaryClinton": "Hiliary Clinton (@HiliaryClinton)",
+               "BernieSanders": "Bernie Sanders (@BernieSanders)",
+               "BarackObama": "Barack Obama (@BarackObama)",
+               "tedcruz": "Ted Cruz (@tedcruz)"
+               }
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',
+                           politicians=POLITICIANS
+                           )
 
 
 @app.route('/results', methods=['GET'])
 def results():
-    return render_template('results.html')
+    twitter_1 = request.args.get('Politician_1')
+    twitter_2 = request.args.get('Politician_2')
+
+    if twitter_1 == twitter_2:
+        return render_template('index.html',
+                               politicians=POLITICIANS
+                               )
+    else:
+        return render_template('results.html',
+                               twitter_1=POLITICIANS[twitter_1],
+                               twitter_2=POLITICIANS[twitter_2]
+                               )
