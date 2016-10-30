0becfb9e49c749aae8022fd6b9a2f64e201cf21f
Akil Darjean <adarjean@uh.edu>
2016-10-29 22:38:58 -0500

    Finished template for first page

--- president_watson/app/views.py
+++ president_watson/app/views.py
@@ -5,5 +5,9 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html',
-                           )
+    return render_template('index.html')
+
+
+@app.route('/results', methods=['GET'])
+def results():
+    return render_template('results.html')
